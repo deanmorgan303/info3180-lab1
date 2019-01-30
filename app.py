d99ad7195ceb0ee8b5a04dcccca960c2a4f97710
dean morgan <dean.morgan@mymona.uwi.edu>
2019-01-30 14:27:47 +0000

    created my first flask app

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
+import os 
 
 from flask import Flask, render_template
-
-app = Flask(__name__)
+app = Flask(__name__) 
 
 '''
 # Routing for your application.
 # Put your routes below this comment
-'''
+''' 
+@app.route('/')
+def home():
+    return 'my home page'
 
 
 @app.errorhandler(404)
@@ -22,4 +25,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=8080)
